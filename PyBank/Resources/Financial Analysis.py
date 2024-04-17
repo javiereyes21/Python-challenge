70658989dfbf3c9e0356644600adedd7ff946707
javiereyes21 <javiereyes21@gmail.com>
2024-04-17 16:36:43 -0700

    First part of challenge done

--- PyBank/Resources/Financial Analysis.py
+++ PyBank/Resources/Financial Analysis.py
@@ -14,8 +14,7 @@ greatest_decrease = ["", 0]
 with open(r"C:\Users\javie\Documents\DataAnalytics\Python-challenge\PyBank\Resources\budget_data.csv",'r') as analysis:
 
     Financial_Analysis= csv.reader(analysis, delimiter=",")
-    print (Financial_Analysis)
-
+    
     next(Financial_Analysis)
 
     
@@ -32,10 +31,22 @@ with open(r"C:\Users\javie\Documents\DataAnalytics\Python-challenge\PyBank\Resou
 
         previous_profit_loss = int(row[1])
 
-        #Greatest Increase
+        # Greatest increase and decrease
+        if profit_change > greatest_increase[1]:
+            greatest_increase = [row[0], profit_change]
+        if profit_change < greatest_decrease[1]:
+            greatest_decrease = [row[0], profit_change]
+
+        previous_profit_loss = int(row[1])
+
+        # Average
+
+        average_change = sum(profit_changes) / len(profit_changes) if len(profit_changes) > 0 else 0
 
         
-        print=("Financial Analysis")
-        print("-----------------------")
-        print("Total Months: ", int(total_months))
-        print("total: ", total)
+print("Financial Analysis")
+print("-----------------------")
+print("Total Months: " + str(total_months))
+print("total: ", total)
+print(f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})")
+print(f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})")
